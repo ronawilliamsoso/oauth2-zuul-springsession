2f75dcba72ae73526c51ca50ab194e263e55fd0f
Wei Wang <wei.wang@zoro.de>
2018-11-10 14:00:31 +0100

    init

---
+++ service-registry/src/test/java/com/kakawait/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,17 @@
+package com.kakawait;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import com.zoro.eureka.ServiceRegistryApplication;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = ServiceRegistryApplication.class)
+public class ServiceDiscoveryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

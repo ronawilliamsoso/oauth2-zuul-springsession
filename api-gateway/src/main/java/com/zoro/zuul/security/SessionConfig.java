ad9b350a916cef66654992b93e6fce51f8e22170
Wei Wang <wei.wang@zoro.de>
2018-11-11 22:13:16 +0100

    no session in uaa

---
+++ api-gateway/src/main/java/com/zoro/zuul/security/SessionConfig.java
@@ -0,0 +1,28 @@
+package com.zoro.zuul.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
+
+@Configuration
+@EnableRedisHttpSession
+public class SessionConfig extends AbstractHttpSessionApplicationInitializer {
+	@Bean
+	public JedisConnectionFactory jedisConnectionFactory() {
+		JedisConnectionFactory redis = new JedisConnectionFactory();
+		redis.setPassword("123456");
+		return redis;
+    }
+
+	@Bean
+	public RedisTemplate<String, Object> redisTemplate() {
+		final RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
+		template.setConnectionFactory(jedisConnectionFactory());
+		template.setValueSerializer(new GenericToStringSerializer<Object>(Object.class));
+		return template;
+	}
+}
